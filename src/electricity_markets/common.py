b081794577ee56040e89a7b6dbf52ebd818edc86
Fernando3161 <fernandoandres.penaherreravaca@offis.de>
2023-07-28 12:19:29 +0200

    fixed common

--- src/electricity_markets/common.py
+++ src/electricity_markets/common.py
@@ -32,5 +32,41 @@ RESULTS_DATA_DIR = join(RESULTS_DIR, "data")
 RESULTS_VIS_DIR = join(RESULTS_DIR, "visualizations")
 
 
+def check_and_create_folders(folders):
+    """
+    Check if the specified folders exist; if not, create them.
+
+    Args:
+        folders (list[str]): A list of folder paths to check and create if missing.
+    """
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            print(f"Folder '{folder}' created.")
+        else:
+            pass
+            # print(f"Folder '{folder}' already exists.")
+
+
+def check_and_create_all_folders():
+    """
+    Check and create all the necessary project folders if missing.
+    """
+    folders_to_check = [
+        BASE_DIR,
+        SOURCE_DIR,
+        ELECTRICITY_MARKETS_DIR,
+        RAW_DATA_DIR,
+        REAL_RAW_DATA_DIR,
+        DATA_DIR,
+        PROC_DATA_DIR,
+        MODEL_DATA_DIR,
+        RESULTS_DIR,
+        RESULTS_DATA_DIR,
+        RESULTS_VIS_DIR,
+    ]
+    check_and_create_folders(folders_to_check)
+
+
 if __name__ == '__main__':
     pass
